15dcfed74df0e83a4c2155ba917ce8ae4d6c3877
sarangnethority <sarangc.nethority@gmail.com>
2026-09-10 11:48:29 +0530

    update

--- src/config/twilioTemplateConfig.ts
+++ src/config/twilioTemplateConfig.ts
@@ -68,7 +68,7 @@ export const TWILIO_TEMPLATE_MAPPINGS: Record<string, string> = {
   'view_report':"HX4d9646554715b009986934820a0ba610",
   'sd':"HX181e8a77619cd6c88e1741bd63ef564a",
   'pdf':"HX8e6b9057e4a1a58fcad2099c98385ae3",
-  'view_report_pdf_img':"HX8054b45bc78b01540a63791b564d4672",
+  'view_report_pdf_img':"HXd16b0df543d7fab5edd4ad1b464c003b",
   'view_report_pdf_img_v2':"HX8ce6e580dbe228fc5cf3127ce0e09306",
 };
 
